f12fc23e6a1b60809f51f2775bd5e4980b1d22bf
Hot Cakes <Hot Cakes@yoyotiddies>
2017-10-12 01:01:18 -0500

    Merge origin/master
    Conflicts:
    	web/alta.jsp

---
+++ src/java/Base/Conexion.java
@@ -0,0 +1,67 @@
+package Base;
+import java.sql.*;
+
+public class Conexion {
+    private String usrDB;
+    private String passDB;
+    private String urlDB;
+    private String driverClassName;
+    
+    private Connection conn = null;
+    private Statement instruction = null;
+    
+    public Conexion(){
+        this.usrDB = "root";
+        this.passDB = "n0m3l0";
+        this.urlDB = "jdbc:mysql://localhost/usuarios";
+        this.driverClassName = "com.mysql.jdbc.Driver";
+    }
+    
+    //Conexion a BD
+    public void Conectar(){
+        try{
+            Class.forName(this.driverClassName).newInstance();
+            conn = DriverManager.getConnection(urlDB, usrDB, passDB);
+        }
+        catch (Exception err){
+            System.out.println("Error "+ err.getMessage());
+        }
+    }
+    
+    //Cerrar conexion a BD
+    public void cierraConexion() throws SQLException{
+        this.conn.close();
+    }
+    
+    //Metodos para ejecutar sentencias SQL
+    public ResultSet consulta (String consulta) throws SQLException {
+        instruction = conn.createStatement();
+        return instruction.executeQuery(consulta);
+    }
+    
+    public void insertar(String inserta) throws SQLException{
+        try {
+            instruction = conn.createStatement();
+            instruction.executeUpdate(inserta);
+            conn.close();
+        }
+        catch (SQLException error){
+            System.out.println(error.toString());
+        }
+        conn.close();
+    }
+    
+    public String getConexion(){
+    Connection cn = null;
+    String msg = "";
+    try{
+        Class.forName(driverClassName);
+        cn = DriverManager.getConnection(urlDB, usrDB, passDB);
+        msg=("Conexion Satisfactoria");
+    }
+    catch (Exception e){
+        msg=("Error de conexion: "+e);
+    }
+    return msg;
+    }
+}
